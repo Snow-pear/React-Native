16a8fa8ba7692a30562935f13c33596e8f0e4026
wangxueli <958427071@qq.com>
2018-09-17 20:50:37 +0800

    渲染第一个电影数据

---
+++ src/SampleAppMovies/App.js
@@ -0,0 +1,100 @@
+import  React,{ Component } from 'react';
+import  {
+    Image,
+    Text,
+    View,
+    StyleSheet }
+    from 'react-native';
+
+let MOCKED_MOVIES_DATA = [
+    {
+        title: '标题',
+        year: '2015',
+        posters: { thumbnail: "http://i.imgur.com/UePbdph.jpg" }
+    }
+];
+let REQUEST_URL =
+    "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
+
+export default class MovieList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: null,
+        };
+        this.fetchData = this.fetchData.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    movies: responseData.movies,
+                });
+            });
+    }
+
+    render() {
+        if (!this.state.movies) {
+            return this.renderLoadingView();
+        }
+
+        let movie = this.state.movies[0];
+        return this.renderMovie(movie);
+    }
+
+    renderLoadingView() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    正在加载电影数据……
+                </Text>
+            </View>
+        );
+    }
+
+    renderMovie(movie) {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: movie.posters.thumbnail}}
+                    style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    thumbnail: {
+        width: 53,
+        height: 81
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+    year: {
+        textAlign: 'center',
+    },
+});
