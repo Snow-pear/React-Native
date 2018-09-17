54d9025323eb7f798579b952ae2fd3ff5118b5e1
wangxueli <958427071@qq.com>
2018-09-17 21:44:32 +0800

    添加FlatList

--- src/SampleAppMovies/App.js
+++ src/SampleAppMovies/App.js
@@ -1,27 +1,20 @@
-import  React,{ Component } from 'react';
-import  {
-    Image,
-    Text,
-    View,
-    StyleSheet }
-    from 'react-native';
 
-let MOCKED_MOVIES_DATA = [
-    {
-        title: '标题',
-        year: '2015',
-        posters: { thumbnail: "http://i.imgur.com/UePbdph.jpg" }
-    }
-];
+import React, { Component } from "react";
+
+import { Image, FlatList, StyleSheet, Text, View } from "react-native";
+
 let REQUEST_URL =
     "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
 
-export default class MovieList extends Component {
+export default class MovieList  extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            movies: null,
+            data: [],
+            loaded: false
         };
+        // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向会变为空
+        // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
         this.fetchData = this.fetchData.bind(this);
     }
 
@@ -31,10 +24,12 @@ export default class MovieList extends Component {
 
     fetchData() {
         fetch(REQUEST_URL)
-            .then((response) => response.json())
-            .then((responseData) => {
+            .then(response => response.json())
+            .then(responseData => {
+                // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
                 this.setState({
-                    movies: responseData.movies,
+                    data: this.state.data.concat(responseData.movies),
+                    loaded: true
                 });
             });
     }
@@ -42,58 +37,67 @@ export default class MovieList extends Component {
     renderLoadingView() {
         return (
             <View style={styles.container}>
-                <Text>
-                    正在加载电影数据……
-                </Text>
+                <Text>Loading movies...</Text>
             </View>
         );
     }
 
-    renderMovie(movie) {
+    renderMovie({ item }) {
+        // { item }是一种“解构”写法，请阅读ES2015语法的相关文档
+        // item也是FlatList中固定的参数名，请阅读FlatList的相关文档
         return (
             <View style={styles.container}>
                 <Image
-                    source={{uri: movie.posters.thumbnail}}
+                    source={{ uri: item.posters.thumbnail }}
                     style={styles.thumbnail}
                 />
                 <View style={styles.rightContainer}>
-                    <Text style={styles.title}>{movie.title}</Text>
-                    <Text style={styles.year}>{movie.year}</Text>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Text style={styles.year}>{item.year}</Text>
                 </View>
             </View>
         );
     }
 
     render() {
-        if (!this.state.movies) {
+        if (!this.state.loaded) {
             return this.renderLoadingView();
         }
-        let movie = this.state.movies[0];
-        return this.renderMovie(movie);
+        return (
+            <FlatList
+                data={this.state.data}
+                renderItem={this.renderMovie}
+                style={styles.list}
+            />
+        );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF"
     },
     rightContainer: {
-        flex: 1,
+        flex: 1
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: "center"
+    },
+    year: {
+        textAlign: "center"
     },
     thumbnail: {
         width: 53,
         height: 81
     },
-    title: {
-        fontSize: 20,
-        marginBottom: 8,
-        textAlign: 'center',
-    },
-    year: {
-        textAlign: 'center',
-    },
+    list: {
+        paddingTop: 20,
+        backgroundColor: "#F5FCFF"
+    }
 });
